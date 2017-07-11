1eae371b47917a4ccc4e70946cae57b051f378bd
Rowan Krishnan <rkrishnan8594@gmail.com>
2017-07-11 13:43:24 -0400

    Cleanup

--- server/app.js
+++ server/app.js
@@ -28,9 +28,5 @@ app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 app.use('/*', staticFiles);
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../../client/build/index.html'));
-// });
-
-app.set('port', (process.env.PORT || 8000))
+app.set('port', (process.env.PORT || 8000));
 app.listen(app.get('port'));

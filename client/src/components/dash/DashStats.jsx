d8fba40a816f9aab9bbc48628bff0634bf7021af
Rowan Krishnan <rkrishnan8594@gmail.com>
2017-08-17 17:08:04 -0400

    Adding responsive layouts for dashboard

--- client/src/components/dash/DashStats.jsx
+++ client/src/components/dash/DashStats.jsx
@@ -5,16 +5,22 @@ export default class DashStats extends Component {
     return (
       <div className="dash-stats">
         <div className="dash-stat">
-          <p className="stat-name">Total contributed.</p>
-          <p className="stat-number">${this.props.user.total.toFixed(2)}</p>
+          <div className="stat-content">
+            <p className="stat-name">Total contributed.</p>
+            <p className="stat-number">${this.props.user.total.toFixed(2)}</p>
+          </div>
         </div>
         <div className="dash-stat">
-          <p className="stat-name">Amount saved this week.</p>
-          <p className="stat-number">${this.props.savedChange.toFixed(2)}</p>
+          <div className="stat-content">
+            <p className="stat-name">Amount saved this week.</p>
+            <p className="stat-number">${this.props.savedChange.toFixed(2)}</p>
+          </div>
         </div>
         <div className="dash-stat">
-          <p className="stat-name">Number of contributions.</p>
-          <p className="stat-number">{this.props.user.numContribs}</p>
+          <div className="stat-content">
+            <p className="stat-name">Number of contributions.</p>
+            <p className="stat-number">{this.props.user.numContribs}</p>
+          </div>
         </div>
       </div>
     );

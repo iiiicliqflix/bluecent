ed734ed7bee6fb5c4195864489dc8f98ba0646ed
Rowan Krishnan <rkrishnan8594@gmail.com>
2017-08-31 19:27:57 -0400

    Updating FAQ

--- client/src/components/FAQ.jsx
+++ client/src/components/FAQ.jsx
@@ -11,28 +11,24 @@ export default class FAQ extends Component {
           <ul className="faq-items">
             <li className="faq-item">
               <p className="faq-question" id="how">How does Bluecent work?</p>
-              <p className="faq-answer">Bluecent rounds up the spare change you make in daily transactions, and at the end of every month donates the sum to Democratic candidates running for Congress in 2018. Connect your bank account to Bluecent after signing up, and the app will immediately begin rounding-up and tallying spare change as you make transactions. Users can also optionally identify particular candidates for US Senate and US House of Representatives that they want to support, and what percentage of their contribution they want going to each candidate.</p>
-              <p className="faq-answer">Then, at the beginning of each month, Bluecent totals contributions for each Democratic candidate and makes a donation directly to their campaign.</p>
+              <p className="faq-answer">Bluecent rounds up spare change from daily financial transactions and at the end of every week donates the sum to Democratic candidates running for Congress in 2018. For example, spending $7.20 on a burrito will add $0.80 to your weekly total. Connect your bank account to Bluecent after signing up and the app will immediately begin rounding up and tallying spare change as you make transactions.</p>
+              <p className="faq-answer">Upcoming features will allow users to select specific campaigns that you want your contributions to support and the percentage of your contribution that will go to each campaign.</p>
             </li>
             <li className="faq-item">
-              <p className="faq-question">What percentage of my contributions go to political campaigns?</p>
-              <p className="faq-answer">One-hundred percent. Users may include small donation to Bluecent with each monthly contribution (which goes towards paying the technical costs of maintaining the app), but the amount of spare change raised by users goes entirely to political campaigns.</p>
+              <p className="faq-question">Is Bluecent a PAC or a nonprofit?</p>
+              <p className="faq-answer">Bluecent is a political action committee registered with the FEC, under the name “Bluecent PAC”. This means that Bluecent is subject to all US federal campaign finance laws that regulate the activity of political action committees and the contributions that they make.</p>
             </li>
             <li className="faq-item">
-              <p className="faq-question">Is Bluecent a PAC or nonprofit?</p>
-              <p className="faq-answer">Bluecent is a traditional PAC registered with the FEC, under the name “Bluecent PAC”. This means that Bluecent is subject to all US federal campaign finance laws that regulate the activity of political committees and the contributions that they make.</p>
-            </li>
-            <li className="faq-item">
-              <p className="faq-question">Who is behind Bluecent?</p>
+              <p className="faq-question">Who works on Bluecent?</p>
               <p className="faq-answer">Bluecent is created and maintained by <a href="http://rowankrishnan.com" className="faq-link">Rowan Krishnan</a>, a software engineer based in Boston, Massachusetts.</p>
             </li>
             <li className="faq-item">
               <p className="faq-question">How do I contact Bluecent?</p>
-              <p className="faq-answer">The best way to get in touch is to send an email to <a href="mailto:rowan@bluecent.org" className="faq-link">rowan@bluecent.org</a>. You can also reach out on <a href="https://www.facebook.com" className="faq-link">Twitter</a> or <a href="https://www.facebook.com" className="faq-link">Facebook</a>. If you notice any bugs or inconsistencies on the site, please let us know!</p>
+              <p className="faq-answer">The best way to get in touch is to send an email to <a href="mailto:rowan@bluecent.org" className="faq-link">rowan@bluecent.org</a>. If you notice any bugs or inconsistencies on the site, please let us know!</p>
             </li>
             <li className="faq-item">
               <p className="faq-question">Can I help or get involved in any way?</p>
-              <p className="faq-answer">We’re always on the lookout for folks to help on the app development side of things. Bluecent is built with Node.js and React, so experience with those technologies is a plus. Check out our <a href="http://github.com/rkrishnan8594/bluecent" className="faq-link">repository</a> on GitHub for more information about how Bluecent is built.</p>
+              <p className="faq-answer">I’m always on the lookout for folks to help work on app development or design. Bluecent is built with Node.js and React, so experience with those technologies is a plus. Check out our <a href="http://github.com/rkrishnan8594/bluecent" className="faq-link">repository</a> on GitHub for more information about how Bluecent is built.</p>
             </li>
           </ul>
         </div>

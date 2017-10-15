5e00ae7e5ac00587f1d5810810a924cb40d58b40
Rowan Krishnan <rkrishnan8594@gmail.com>
2017-10-15 16:43:35 -0400

    First home page design changes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,13 +6,14 @@ export default class Home extends Component {
     return (
       <div className="container-home">
         <h1 className="hdr">Every cent counts.</h1>
+        <p className="sub-hdr">Donate spare change to Democrats running for Congress in 2018.</p>
         <div className="btns">
           <Link to="/signup">
             <button className="get-started">Get Started</button>
           </Link>
-          <a href="/faq#how">
+          {/*<a href="/faq#how">
             <button className="get-started">How It Works</button>
-          </a>
+          </a> */}
         </div>
       </div>
     );
--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -7,47 +7,37 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.closeAlert = this.closeAlert.bind(this);
-    this.state = { showBetaAlert: true };
   }
 
   handleClick() {
     this.props.signOut();
   }
 
-  closeAlert() {
-    this.setState({ showBetaAlert: false });
-  }
-
   render() {
     return (
       <header>
-        <h1 className="logo">
-          <Link to={this.props.authenticated ? '/dashboard' : '/'}>bluecent</Link>
-        </h1>
-        {/*{this.state.showBetaAlert &&
-          <div className="beta-alert">
-            <span>We're currently in beta!</span>
-            <span className="close-alert" onClick={this.closeAlert}>×</span>
-          </div>
-        }*/}
-        <ul className="nav">
-          <li className="nav-item">
-            <Link to="/faq">FAQ</Link>
-          </li>
-          <li className="nav-item">
-            <a href="https://github.com/rkrishnan8594/bluecent">Open Source</a>
-          </li>
-          {this.props.authenticated ?
-            <li className="nav-item login" onClick={this.handleClick}>
-              <Link to="#" className="signout-link">Sign Out</Link>
+        <div className="header-content">
+          <h1 className="logo">
+            <Link to={this.props.authenticated ? '/dashboard' : '/'}>bluecent</Link>
+          </h1>
+          <ul className="nav">
+            <li className="nav-item">
+              <Link to="/faq">FAQ</Link>
             </li>
-          :
-            <li className="nav-item login">
-              <Link to="/login" className="login-link">Login</Link>
+            <li className="nav-item">
+              <a href="https://github.com/rkrishnan8594/bluecent">Open Source</a>
             </li>
-          }
-        </ul>
+            {this.props.authenticated ?
+              <li className="nav-item login" onClick={this.handleClick}>
+                <Link to="#" className="signout-link">Sign Out</Link>
+              </li>
+            :
+              <li className="login">
+                <Link to="/login" className="login-link">Login</Link>
+              </li>
+            }
+          </ul>
+        </div>
       </header>
     )
   }
